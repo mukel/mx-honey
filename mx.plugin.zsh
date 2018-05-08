ac5c728f83c48cbbf2e3867a05b8e863103e6704
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2018-05-08 17:54:56 +0200

    Add simple mx completions.

---
+++ mx.plugin.zsh
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+#          FILE:  mx.plugin.zsh
+#   DESCRIPTION:  zsh plugin file
+#        AUTHOR:  Alfonso2 Peterssen (a2peterssen@gmail.com)
+#       VERSION:  0.1
+# ------------------------------------------------------------------------------
+ 
+# aliases - mnemonic: prefix is 'mx'
+alias mxb='mx build'
+alias mxbe='mx benchmark'
+alias mxcl='mx clean'
+alias mxcs='mx checkstyle'
+alias mxf='mx eclipseformat'
+alias mxu='mx unittest'
+alias mxdu='mx -d unittest'
+alias mxupd='mx update'
